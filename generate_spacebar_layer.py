d3eaf6eac02ab5b36559cf3924cd190bdf2afb3e
Anjie Li <lenwhite@users.noreply.github.com>
2025-04-16 20:28:31 +0100

    Simplify code and fix edge case

--- generate_spacebar_layer.py
+++ generate_spacebar_layer.py
@@ -30,7 +30,7 @@ SET_KEY_DOWN = {"set_variable": {"name": LAYER_KEY_DOWN, "value": 1}}
 SET_KEY_UP = {"set_variable": {"name": LAYER_KEY_DOWN, "value": 0}}
 SET_EFFECT_TRIGGERED = {"set_variable": {"name": LAYER_EFFECT_TRIGGERED, "value": 1}}
 
-SEND_SPACEBAR_KEYCODE = {"key_code": "spacebar", "halt": True}
+SEND_SPACEBAR_KEYCODE_AND_HALT = {"key_code": "spacebar", "halt": True}
 
 ACTIVATE_LAYER = {"set_variable": {"name": SPACEBAR_DOWN, "value": 1}}
 DEACTIVATE_LAYER = {"set_variable": {"name": SPACEBAR_DOWN, "value": 0}}
@@ -43,45 +43,21 @@ def generate_spacebar_manipulator():
         "from": {"key_code": "spacebar", "modifiers": {"optional": ["any"]}},
         "to_if_alone": [
             DEACTIVATE_LAYER,
-            SEND_SPACEBAR_KEYCODE,
+            SEND_SPACEBAR_KEYCODE_AND_HALT,
         ],
         "to": [
             ACTIVATE_LAYER,
-            {"set_variable": {"name": LAYER_KEY_DOWN, "value": 0}},
-            {"set_variable": {"name": LAYER_EFFECT_TRIGGERED, "value": 0}},
         ],
         "to_after_key_up": [
             DEACTIVATE_LAYER,
-            # fire spacebar late if it's not triggered because it was
-            # interrupted by a triggering key press
-            {
-                "conditions": [
-                    IF_LAYER_KEY_DOWN,
-                    IF_NOT_LAYER_EFFECT_TRIGGERED,
-                ],
-                **SEND_SPACEBAR_KEYCODE,
-            },
         ],
         "to_delayed_action": {
             "to_if_canceled": [
-                # interrupted by a non-layer effect key press
-                # release the layer effect and fire spacebar immediately
-                {
-                    "conditions": [
-                        IF_NOT_LAYER_KEY_DOWN,
-                        IF_NOT_LAYER_EFFECT_TRIGGERED,
-                    ],
-                    **DEACTIVATE_LAYER,
-                },
-                {
-                    "conditions": [
-                        IF_NOT_LAYER_KEY_DOWN,
-                        IF_NOT_LAYER_EFFECT_TRIGGERED,
-                    ],
-                    **SEND_SPACEBAR_KEYCODE,
-                },
+                DEACTIVATE_LAYER,
+                SEND_SPACEBAR_KEYCODE_AND_HALT,
             ]
         },
+        "parameters": {"basic.to_delayed_action_delay_milliseconds": 500},
     }
 
 
@@ -90,7 +66,6 @@ def generate_simultaneous_manipulator(from_key, to_key, to_modifiers=None):
 
     SEND_REMAPPED_KEY = {
         "key_code": to_key,
-        **HALT,
     }
 
     if to_modifiers:
@@ -113,7 +88,7 @@ def generate_simultaneous_manipulator(from_key, to_key, to_modifiers=None):
             ACTIVATE_LAYER,
             SEND_REMAPPED_KEY,
         ],
-        "parameters": {"basic.simultaneous_threshold_milliseconds": 500},
+        "parameters": {"basic.simultaneous_threshold_milliseconds": 200},
     }
 
 
@@ -129,32 +104,10 @@ def generate_layer_key_manipulator(from_key, to_key, to_modifiers=None):
     if to_modifiers:
         SEND_REMAPPED_KEY["modifiers"] = to_modifiers
 
-    to_effect = [
-        {
-            "conditions": [IF_LAYER_ACTIVE],
-            **SET_EFFECT_TRIGGERED,
-        },
-        SEND_REMAPPED_KEY,
-        {
-            "conditions": [IF_NOT_LAYER_ACTIVE],
-            "key_code": from_key,
-            **HALT,
-        },
-    ]
-
     return {
         "type": "basic",
         "from": {"key_code": from_key, "modifiers": {"optional": ["any"]}},
-        "to_if_held_down": to_effect,
-        "to": [
-            SET_KEY_DOWN,
-            {"set_variable": {"name": LAYER_EFFECT_TRIGGERED, "value": 0}},
-        ],
-        "to_after_key_up": [
-            SET_KEY_UP,
-            *to_effect,
-        ],
-        "to_delayed_action": {"to_if_canceled": to_effect},
+        "to": [SEND_REMAPPED_KEY],
         "conditions": [{"type": "variable_if", "name": SPACEBAR_DOWN, "value": 1}],
     }
 
@@ -165,20 +118,20 @@ def main():
     mappings = {
         # Arrows
         "k": ("left_arrow", None),
-        # "o": ("up_arrow", None),
-        # "semicolon": ("right_arrow", None),
-        # "l": ("down_arrow", None),
-        # # Word navigation
-        # "i": ("left_arrow", ["left_option"]),
-        # "p": ("right_arrow", ["left_option"]),
-        # # line/page navigation
-        # "a": ("home", None),
-        # "d": ("end", None),
-        # "w": ("page_up", None),
-        # "s": ("page_down", None),
-        # # select words
-        # "q": ("left_arrow", ["left_shift", "left_option"]),
-        # "e": ("right_arrow", ["left_shift", "left_option"]),
+        "o": ("up_arrow", None),
+        "semicolon": ("right_arrow", None),
+        "l": ("down_arrow", None),
+        # Word navigation
+        "i": ("left_arrow", ["left_option"]),
+        "p": ("right_arrow", ["left_option"]),
+        # line/page navigation
+        "a": ("home", None),
+        "d": ("end", None),
+        "w": ("page_up", None),
+        "s": ("page_down", None),
+        # select words
+        "q": ("left_arrow", ["left_shift", "left_option"]),
+        "e": ("right_arrow", ["left_shift", "left_option"]),
     }
 
     manipulators = []
